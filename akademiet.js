ac2d36a9e980c550ad240d6776cffe7d13f0c5c6
“FreakyBoo” <julianriversandvikendelgado@gmail.com>
2024-11-06 09:49:08 +0100

    Jeg lærte om `<span>`-taggen for å bruke inline styling på tekst og små elementer, og la til denne i menyseksjonen for å formatere pris og navn separat - Brukte `position: fixed;` på footeren for å sikre at den alltid vises nederst på siden, uavhengig av innholdets lengde - Justerte `bottom`, `width`, og `z-index` i CSS for å få footeren til å fungere responsivt og forbli synlig - Oppdaterte navigasjonsknapper og hover-effekter for bedre brukeropplevelse - Forbedret layout og legg til mørk modus for en bedre brukeropplevelse

---
+++ akademiet.js
@@ -0,0 +1,5 @@
+// Ta knappen og bruk en addEventListener kode for å få click funksjon
+document.getElementById('darkModeToggle').addEventListener('click', function() {
+    // Toggle mørke modus på nettside
+    document.body.classList.toggle('dark-mode');
+});
